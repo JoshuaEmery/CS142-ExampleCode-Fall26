211b8edd11d4475d9e9091ad3da60ab9d606bdc5
Joshua Emery <38080740+JoshuaEmery@users.noreply.github.com>
2026-09-21 10:51:23 -0700

    Add files via upload

---
+++ week1/IntroToJava.java
@@ -0,0 +1,25 @@
+//This is a comment
+//They are ignored by the compiler
+//It is a way to leave notes for yourself or others
+
+/*
+ * This is a multi-line comment
+ * I can hit enter
+ * and keep typing and it
+ * all stays in the comment
+ */
+
+
+public class IntroToJava {//This is the start of the class
+
+	public static void main(String[] args) {//This is the start of main method
+		//the main method is what runs when I run my application
+		//sysout prints whatever is in quotes to the console, makes a new line
+		System.out.println("Hello World");
+		System.out.println("This is my first Java Program");
+		System.out.print("print does not ");
+		System.out.print("make a new line");
+
+	}//This is the end of the main method
+
+}//This is the end of the class
